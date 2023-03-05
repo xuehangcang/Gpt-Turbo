66f14b6ee0bee94509aab37ff2b36cc4fa004bc9
Xuehang Cang <xuehangcang@outlook.com>
2023-03-06 01:34:36 +0800

    feat:添加跨域

--- code/index.py
+++ code/index.py
@@ -8,9 +8,15 @@ import markdown
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:9000",
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
