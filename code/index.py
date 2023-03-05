48debebcca54ff55e02b12bc4b066e17061ece6b
Xuehang Cang <xuehangcang@outlook.com>
2023-03-06 01:29:42 +0800

    feat:添加跨域

--- code/index.py
+++ code/index.py
@@ -5,8 +5,16 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import openai
 import markdown
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
