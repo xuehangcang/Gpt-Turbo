fc59921086971ea9623210ce411bcbf1359f73e1
Xuehang Cang <xuehangcang@outlook.com>
2023-03-06 01:07:49 +0800

    feat:适配安卓浏览器

--- code/index.py
+++ code/index.py
@@ -1,24 +1,41 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 import openai
+import markdown
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-@app.get('/')
-async def gpt(api_key, user_content: str, system_content: str = "", assistant_content: str = ""):
-    """基于 fastapi 的 gpt-3.5-turbo 项目, 用于快速部署,方便API请求调试"""
-    openai.api_key = api_key
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post('/submit-form')
+async def submit_form(request: Request, user_content: str = Form(...)):
+    openai.api_key = "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"
     results = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": system_content},
+            {"role": "system", "content": ""},
             {"role": "user", "content": user_content},
-            {"role": "assistant", "content": assistant_content},
+            {"role": "assistant", "content": ""},
         ]
     )
-    return results
+    content = results["choices"][0]["message"]["content"]
+    content = markdown.markdown(content,
+                                extensions=[
+                                    'markdown.extensions.extra',
+                                    'markdown.extensions.codehilite',
+                                ])
+    return templates.TemplateResponse("result.html", {"request": request, "content": content})
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=9000)
+    uvicorn.run("index:app", host="0.0.0.0", port=9000, reload=True)
